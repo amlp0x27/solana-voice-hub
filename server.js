e4a57b7f40baa2ed2133337b8e7fa9c1e2c2439e
amlp0x27 <amlp0x27@gmail.com>
2025-02-27 13:23:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,132 @@ const { Server } = require('socket.io');
 const { ExpressPeerServer } = require('peer');
 const app = express();
 const server = app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-  console.log(`[Server v2] Server running on port ${process.env.PORT || 3000}`);
+  console.log(`[Server v7] Server running on port ${process.env.PORT || 3000}`);
 });
 const io = new Server(server, { cors: { origin: '*' } });
 app.use(express.static(__dirname));
 const peerServer = ExpressPeerServer(server, { path: '/peerjs' });
 app.use('/peerjs', peerServer);
 
+const rooms = {};
+const profanityList = ['damn', 'hell', 'ass', 'fuck', 'shit', 'bitch', 'cunt', 'bastard'];
+
+function filterText(text) {
+  let cleaned = text;
+  profanityList.forEach(word => {
+    const regex = new RegExp(`\\b${word}\\b`, 'gi');
+    cleaned = cleaned.replace(regex, '****');
+  });
+  return cleaned;
+}
+
 io.on('connection', (socket) => {
-  console.log('[Server v2] User connected:', socket.id);
+  console.log(`[Server v7] User connected: ${socket.id}`);
+
+  socket.on('userConnected', ({ peerId, username, avatar }) => {
+    socket.username = filterText(username);
+    socket.avatar = avatar;
+    socket.peerId = peerId;
+    console.log(`[Server v7] User ${socket.username} connected with peerId: ${peerId}`);
+    io.emit('roomUpdate', { totalUsers: io.engine.clientsCount, users: [], messages: [] });
+  });
 
   socket.on('joinRoom', ({ address, peerId, avatar, username }) => {
-    console.log(`[Server v2] Join request: ${address}, peerId: ${peerId}, username: ${username}`);
+    console.log(`[Server v7] Join: ${address}, peerId: ${peerId}, username: ${username}`);
     socket.join(address);
-    const roomUsers = [...io.sockets.adapter.rooms.get(address) || []].map(id => ({
-      id,
-      username: id === socket.id ? username : 'User',
-      avatar: id === socket.id ? avatar : 'https://via.placeholder.com/50',
-      peerId: id === socket.id ? peerId : null
-    }));
-    io.to(address).emit('roomUpdate', { address, users: roomUsers });
-    console.log(`[Server v2] Room update sent to ${address}:`, roomUsers);
+    socket.address = address;
+
+    if (!rooms[address]) rooms[address] = { users: [], messages: [], typing: [] };
+    const cleanUsername = filterText(username);
+    const user = { id: socket.id, username: cleanUsername, avatar, peerId, muted: false };
+    rooms[address].users = rooms[address].users.filter(u => u.id !== socket.id);
+    rooms[address].users.push(user);
+
+    const roomData = {
+      address,
+      users: rooms[address].users,
+      messages: rooms[address].messages,
+      totalUsers: io.engine.clientsCount
+    };
+    io.to(address).emit('roomUpdate', roomData);
+    io.to(address).emit('userJoined', { id: socket.id, avatar, username: cleanUsername });
+    console.log(`[Server v7] Room ${address} updated:`, roomData);
+  });
+
+  socket.on('chatMessage', ({ address, message, username }) => {
+    const cleanMessage = filterText(message);
+    if (rooms[address]) {
+      rooms[address].messages.push({ username, text: cleanMessage, timestamp: Date.now() });
+      io.to(address).emit('newMessage', rooms[address].messages);
+      console.log(`[Server v7] Message in ${address}: ${username}: ${cleanMessage}`);
+    }
+  });
+
+  socket.on('typing', ({ address, username }) => {
+    if (rooms[address]) {
+      rooms[address].typing = [username];
+      io.to(address).emit('typingUpdate', rooms[address].typing);
+    }
+  });
+
+  socket.on('stopTyping', ({ address }) => {
+    if (rooms[address]) {
+      rooms[address].typing = [];
+      io.to(address).emit('typingUpdate', rooms[address].typing);
+    }
+  });
+
+  socket.on('muteUser', ({ address, targetId }) => {
+    if (rooms[address]) {
+      const user = rooms[address].users.find(u => u.id === targetId);
+      if (user) {
+        user.muted = !user.muted;
+        io.to(address).emit('roomUpdate', { address, users: rooms[address].users, messages: rooms[address].messages });
+      }
+    }
+  });
+
+  socket.on('voteKick', ({ address, targetId }) => {
+    if (rooms[address]) {
+      rooms[address].votes = rooms[address].votes || {};
+      rooms[address].votes[targetId] = (rooms[address].votes[targetId] || 0) + 1;
+      io.to(address).emit('voteUpdate', { targetId, votes: rooms[address].votes[targetId] });
+      if (rooms[address].votes[targetId] >= 10) {
+        io.to(address).emit('userKicked', targetId);
+        const socketToKick = io.sockets.sockets.get(targetId);
+        if (socketToKick) socketToKick.disconnect(true);
+        rooms[address].users = rooms[address].users.filter(u => u.id !== targetId);
+        delete rooms[address].votes[targetId];
+      }
+    }
+  });
+
+  socket.on('leaveRoom', ({ address, peerId }) => {
+    if (rooms[address]) {
+      rooms[address].users = rooms[address].users.filter(u => u.peerId !== peerId);
+      io.to(address).emit('userLeft', socket.id);
+      io.to(address).emit('roomUpdate', {
+        address,
+        users: rooms[address].users,
+        messages: rooms[address].messages,
+        totalUsers: io.engine.clientsCount
+      });
+    }
+    socket.leave(address);
   });
 
   socket.on('disconnect', () => {
-    console.log('[Server v2] User disconnected:', socket.id);
+    console.log(`[Server v7] User disconnected: ${socket.id}`);
+    if (socket.address && rooms[socket.address]) {
+      rooms[socket.address].users = rooms[socket.address].users.filter(u => u.id !== socket.id);
+      io.to(socket.address).emit('userLeft', socket.id);
+      io.to(socket.address).emit('roomUpdate', {
+        address: socket.address,
+        users: rooms[socket.address].users,
+        messages: rooms[socket.address].messages,
+        totalUsers: io.engine.clientsCount
+      });
+    }
+    io.emit('roomUpdate', { totalUsers: io.engine.clientsCount });
   });
 });
